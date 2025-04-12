b0d8b1e2772de19117eb91df288a40d3ebc7f220
Lucimar Ferreira <lucimar.silvasr61@gmail.com>
2025-04-12 19:58:35 -0300

    Tela de cadastro de clientes

---
+++ sistema_aralev-master/sistema_aralev-master/javascript/frontProdutos.js
@@ -0,0 +1,7 @@
+import { API_URL } from './api.js';
+
+document.getElementById("open_popUp").addEventListener("click", async function (event) {
+    // evita o carregamento da página
+    event.preventDefault();
+    
+})
